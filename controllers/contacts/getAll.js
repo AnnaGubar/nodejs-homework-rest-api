f9aac8367ba493c9469f18471751941dccf877bb
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2022-08-13 18:22:05 +0300

    does pagination

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,9 +1,11 @@
 const { Contact } = require("../../models/contact");
 
 const getAll = async (req, res) => {
-  const {id: owner} = req.user;
+  const {page = 1, limit = 2} = req.query;
+  const skip = (page - 1) * limit;
 
-  const result = await Contact.find({owner}, "-createdAt -updatedAt").populate("owner", "email");
+  const {id: owner} = req.user;
+  const result = await Contact.find({owner}, "-createdAt -updatedAt", {skip, limit: Number(limit)}).populate("owner", "email");
   
   res.status(200).json(result);
 };
