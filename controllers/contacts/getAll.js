8664ab571d7b9a5180446fa4297892d9dc9c0cc8
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2022-08-13 18:59:38 +0300

    adds filter qwery

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,13 +1,25 @@
 const { Contact } = require("../../models/contact");
 
 const getAll = async (req, res) => {
-  const {page = 1, limit = 2} = req.query;
-  const skip = (page - 1) * limit;
+  const { id: owner } = req.user;
 
-  const {id: owner} = req.user;
-  const result = await Contact.find({owner}, "-createdAt -updatedAt", {skip, limit: Number(limit)}).populate("owner", "email");
+  // фильтр по параметру запроса ?favorite=true  
+  const { favorite } = req.query;
+  if (favorite) {
+    const result = await Contact.find({ owner }, "-createdAt -updatedAt");
+    const resultFavorite = [...result].filter((item) => item.favorite);
+
+    res.status(200).json(resultFavorite);
+  }
+
+  // пагинация по параметру запроса ?page=3&limit=2
+  const {page = 1, limit = 2} = req.query;
+  if(page && limit){
+    const skip = (page - 1) * limit;
+    const result = await Contact.find({owner}, "-createdAt -updatedAt", {skip, limit: Number(limit)}).populate("owner", "email");
   
-  res.status(200).json(result);
+    res.status(200).json(result);
+  }
 };
 
 module.exports = getAll;
