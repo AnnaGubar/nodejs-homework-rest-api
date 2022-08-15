27c0b5a37c827f5117e3f5282cd884eb61cdcd3c
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2022-08-15 15:21:09 +0300

    adds creating a random avatar after signup

--- controllers/users/signup.js
+++ controllers/users/signup.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
 
 const { User, registerSchema } = require("../../models/user");
 const { createError } = require("../../helpers");
@@ -21,7 +22,9 @@ const register = async (req, res) => {
 
   const hashPassword = await bcrypt.hash(password, 10);
 
-  const result = await User.create({ ...req.body, password: hashPassword });
+  const avatarURL = gravatar.url(email);
+
+  const result = await User.create({ ...req.body, password: hashPassword,avatarURL });
 
   res.status(201).json({
     email: result.email,
