e1d610b11c54198883c8d5173565fe2495a6458e
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2022-08-15 20:43:56 +0300

    ready to test login.js

---
+++ controllers/users/login.test.js
@@ -0,0 +1,13 @@
+// const mongoose = require("mongoose");
+// const request = require("supertest");
+// require("dotenv").config();
+
+// const app = require("../../app");
+// const { User } = require("../../models/user");
+
+// const { DB_HOST, PORT } = process.env;
+// // console.log(process.env);
+
+// describe("test login-route", () => {
+
+// });
