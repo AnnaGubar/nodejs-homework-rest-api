6ddad7c678751491b9b59b57c580e23e687b128b
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2022-08-15 15:12:07 +0300

    adds "public" folder and configures distribution

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger)); 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public")); // http://localhost:3000/avatars/example.jpg
 
 app.use("/api/users", usersRouter);
 app.use("/api/contacts", contactsRouter);
